c7de8bc48f1fba6d68e753374ea9907ac945f5cd
chandan <chandan.rajbhar54@gmail.com>
2020-11-26 12:59:28 +0530

    get

--- hribernet_tutoriyal/src/com/chandan/controller/test.java
+++ hribernet_tutoriyal/src/com/chandan/controller/test.java
@@ -1,5 +1,7 @@
 package com.chandan.controller;
 
+import java.io.File;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -12,21 +14,42 @@ public class test {
 		//load conf file
 		Configuration cf =new Configuration();
 		cf.configure("hiber.cfg.xml");
+		//cf.configure(new File("C:\\user\\heart\\desktop\\hiber.cfg.xml"));
 		System.out.println(cf);
 			
 		SessionFactory sf= cf.buildSessionFactory();
 		 System.out.println(sf);
-		 
-		 Teacher t1=new Teacher();
-		 t1.setId(4);
-		 t1.setName("Parvind");
-		 t1.setCompany("CG");
+		 //Transient sate 
+		 //thant means object not accosiate with session 
+			/*
+			 * Teacher t1=new Teacher(); t1.setId(4); t1.setName("Parvind");
+			 * t1.setCompany("CG");
+			 */
 		 
 		 Session ss=sf.openSession();
+		 //persitances sate
+		 //whwn object accosiate with  session 
+		 
 		 //ss.persist(t1);
 		 //ss.save(t1);
-		 ss.saveOrUpdate(t1);
+		 //ss.saveOrUpdate(t1);
+		 
+		Teacher tt= ss.get(Teacher.class, 6);
+		
+		
+		if(tt!=null )
+		{
+			System.out.println(tt.getName()+"\t"+tt.getCompany());
+		}else {
+			System.out.println("data not found");
+		}
+		
+		 
 		 ss.beginTransaction().commit();
+		 //detaches sate
+		 //when close the session then object is detached state
+		 ss.close();
+		 
 		 System.out.println("Succfull insert");
 	}
 
